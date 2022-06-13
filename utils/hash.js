c2a5ca7fdcf62e8ec8ed3bfe019bb044ae73c373
Molpe <fran.mscr@gmail.com>
2022-06-13 23:56:16 +0200

    feat: added hash utils

---
+++ utils/hash.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcrypt");
+
+const encrypt = async (password) => {
+  const rounds = 10;
+  const salt = await bcrypt.genSalt(rounds);
+  return await bcrypt.hash(password, salt);
+};
+
+module.exports = {
+  encrypt,
+};
