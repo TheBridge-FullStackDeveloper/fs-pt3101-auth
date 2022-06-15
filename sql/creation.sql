bb93a7385279457d18264b7124d728191d212c51
Molpe <fran.mscr@gmail.com>
2022-06-15 22:48:26 +0200

    chore: added some modifications and added some package

--- sql/creation.sql
+++ sql/creation.sql
@@ -3,5 +3,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  username TEXT,
+  birthdate DATE,
+  bio TEXT
 );
