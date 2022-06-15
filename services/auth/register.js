4149ba921d5bad67d66d9aa38092e22c2508530b
Molpe <fran.mscr@gmail.com>
2022-06-15 22:51:02 +0200

    chore: refactora little register controller

--- services/auth/register.js
+++ services/auth/register.js
@@ -1,12 +1,10 @@
 const { createUser } = require("../../queries/auth");
 const { hash } = require("../../utils");
-const { generic, register } = require("../../errors/auth");
+const { register } = require("../../errors/auth");
 const errors = require("../../errors/commons");
 
 module.exports = (db) => async (req, res, next) => {
   const { email, password } = req.body;
-
-  if (!email || !password) return next(generic["empty"]);
 
   const queryResult = await createUser(db)({
     email,
