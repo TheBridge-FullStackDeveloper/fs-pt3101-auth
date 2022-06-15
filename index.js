b904ec57cea30c98171bb64e2a841bd98311b97f
Molpe <fran.mscr@gmail.com>
2022-06-15 22:49:22 +0200

    chore: added cookie parser in app middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const db = require("./configs/db");
 const errors = require("./errors/commons");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", require("./services")(db));
 
