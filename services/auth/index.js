d768964fffe4ee1828329af1800ede62805f3213
Molpe <fran.mscr@gmail.com>
2022-06-15 22:50:23 +0200

    feat: added logout controller

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const { authorizer, checker } = require("../../middlewares");
+
+const forms = {
+  register: ['email', 'password'],
+  login: ['email', 'password'],
+}
 
 module.exports = (db) => {
-  router.post("/register", require("./register")(db));
-  router.post("/login", require("./login")(db));
+  router.post("/register", checker(...forms.register), require("./register")(db));
+  router.post("/login", checker(...forms.login), require("./login")(db));
+  router.post("/logout", authorizer, require("./logout")());
 
   return router;
 };
