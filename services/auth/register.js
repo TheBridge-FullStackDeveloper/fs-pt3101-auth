bc3958d34934b41a54e4e8cc239ca818f474e3a9
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-06-16 20:34:29 +0200

    Añadida funcionalidad para enviar emails con nodemailer

--- services/auth/register.js
+++ services/auth/register.js
@@ -1,9 +1,9 @@
-const { createUser } = require("../../queries/auth");
-const { hash } = require("../../utils");
-const { register } = require("../../errors/auth");
-const errors = require("../../errors/commons");
+const { createUser } = require('../../queries/auth');
+const { hash, sendEmail } = require('../../utils');
+const { register } = require('../../errors/auth');
+const errors = require('../../errors/commons');
 
-module.exports = (db) => async (req, res, next) => {
+module.exports = db => async (req, res, next) => {
   const { email, password } = req.body;
 
   const queryResult = await createUser(db)({
@@ -12,6 +12,18 @@ module.exports = (db) => async (req, res, next) => {
   });
 
   if (!queryResult.ok) return next(register[queryResult.code] || errors[500]);
+
+  const message = 'Gracias por registrarte en mi web, disfruta el contenido';
+
+  try {
+    await sendEmail({
+      email,
+      subject: 'Bienvenido a mi web, gracias por registrarte',
+      message,
+    });
+  } catch (error) {
+    console.log('> error: ', error.message);
+  }
 
   res.status(200).json({
     success: true,
